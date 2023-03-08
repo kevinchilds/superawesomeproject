1d04188123a715bc5664848a24bbb5826d51c74b
Kevin Childs <kchilds2020@gmail.com>
2023-03-08 16:31:34 -0600

    retrieves num and checks if even or odd

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a num: ");
+
+        String input = sc.nextLine();
+
+        int num;
+
+        try{
+            num = Integer.parseInt(input);
+        }catch(Exception e){
+            System.out.println("invalid input...");
+            return;
+        }
+        
+        if(num % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+
     }
 }
